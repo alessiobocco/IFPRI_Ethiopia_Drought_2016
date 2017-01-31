97a25254a54fe207b3eac4622b5ddd0cd37cea67
Michael Mann <MMann@cloud.ccas.gwu.edu>
2017-01-31 03:39:42 -0500

    adding 2010 data

--- DownloadMODISFTP_Rcurl.R
+++ DownloadMODISFTP_Rcurl.R
@@ -50,7 +50,7 @@ lapply(1:length(functions_in), function(x){cmpfun(get(functions_in[[x]]))})  # b
 
 
   # give path to Modis Reproduction Tool
-  MRT = 'G:/Faculty/Mann/Projects/MRT/bin'
+  MRT = 'H:/Projects/MRT/bin'
   
   # get list of all available modis products
   #GetProducts()
@@ -173,21 +173,22 @@ lapply(1:length(functions_in), function(x){cmpfun(get(functions_in[[x]]))})  # b
 # Reproject ---------------------------------------------------------------
 
   
-  band_subset = "0 0 0 0 0 0 0 0 0 0 1 0"  # EVI 1 1 0 0 0 0 0 0 0 0 1 1# Example: first seven and last layer'1 1 1 1 1 1 1 0 0 0 0 1" landcover= "1 1 0 0 1 1 1 0 0 1 1 0 0 0 0 0"
+  band_subset = "1 1 0 0 0 0 0 0 0 0 1 1"  # EVI 1 1 0 0 0 0 0 0 0 0 1 1# Example: first seven and last layer'1 1 1 1 1 1 1 0 0 0 0 1" landcover= "1 1 0 0 1 1 1 0 0 1 1 0 0 0 0 0"
   output_pattern = '250m_16_days_EVI.tif' # '250m_16_days_EVI.tif' looks for existing EVI tif files to avoid repeating  Land_Cover_Type_1.tif
    
+  tifs = list.files(getwd(),pattern =  output_pattern)
+  
   for (i in (1:length(files$reproj_files))){
       print(i)
       print(paste(i,'out of',length(files$reproj_files)))
       print(paste("Writing out tiffs ", list.files('.',pattern =  files$reproj_files[i] ),' for date ',files$yeardoy[i]))
-      tifs = list.files(getwd(),pattern =  output_pattern)
       
       if(length(tifs[grep(tifs,pattern=paste(files$products[i],files$yeardoy[i],files$tiles[i],sep='_'))])>=1){ print('File exists')
           next
       }else{
           print(paste('Input:',files$reproj_files[i],' Output:',paste(files$products[i],'_',files$yeardoy[i],'.tif',sep='')))
           reprojectHDF(hdfName = files$short_name[i],
-                     filename=paste(files$products[i],'_',files$yeardoy[i],'_',files$tiles[i],'.tif',sep=''),  
+                     filename=  file.path(getwd(),paste(files$products[i],'_',files$yeardoy[i],'_',files$tiles[i],'.tif',sep='')),  
                      MRTpath=MRT, proj_type='SIN', 
                      proj_params='6371007.181 0 0 0', 
                      datum='NODATUM', pixel_size=250,
